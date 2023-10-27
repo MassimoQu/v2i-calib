5e63bc65610768c8355ddf599265e046d6270346
MassimoQu <1061281310@qq.com>
2023-10-27 10:53:26 +0800

    1

--- object/reader/BBoxReader.py
+++ object/reader/BBoxReader.py
@@ -1,10 +1,15 @@
+import os.path as osp
+
 from reader.Reader import Reader
 from object import Reader
 
 
 class BBoxReader(Reader):
-    def __init__(self, file_path):
-        super().__init__(file_path)
+    def __init__(self, folder_name, file_name):
+
+        self.file_path = osp.join(folder_name, file_name)
+        
+        super().__init__(self.file_path)
     
     def read(self):
         with open(self.file_path, 'r') as f:
